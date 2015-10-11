050df57baddfe4ad39b7bdd57d344209f27954ef
Rob Calcroft <rob@calcroft.com>
2015-10-11 14:13:36 +0100

    Mid fixing 'transaction_id' issue on /auth/decision

--- lib/models/sql/Client.sql
+++ lib/models/sql/Client.sql
@@ -1,7 +1,7 @@
 CREATE TABLE "Client" (
     "applicationName" varchar(255) NOT NULL UNIQUE,
-    "applicationURL" varchar(25) NOT NULL UNIQUE,
-    "redirectURI" varchar(25) NOT NULL,
+    "applicationURL" varchar(50) NOT NULL UNIQUE,
+    "redirectURI" varchar(50) NOT NULL,
     "clientId" varchar(100) NOT NULL PRIMARY KEY UNIQUE,
     "clientSecret" varchar(100) NOT NULL UNIQUE,
     "dateCreated" timestamp DEFAULT CURRENT_TIMESTAMP
--- lib/models/sql/dummy/client-data.sql
+++ lib/models/sql/dummy/client-data.sql
@@ -1,5 +1,6 @@
 INSERT INTO "Client" (
     "applicationName",
+    "redirectURI",
     "applicationURL",
     "clientId",
     "clientSecret"
@@ -7,6 +8,7 @@ INSERT INTO "Client" (
 VALUES (
     'testApp',
     'http://www.testapp.com/callback',
+    'http://www.testapp.com',
     'bcb5277f-e6af-4385-9aeb-6523a3bf40f8',
     'e6ce3c35-70a8-448c-a68a-3db71a9dbf90'
 );
