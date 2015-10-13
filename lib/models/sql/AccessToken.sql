835cab25e9ead8fb80a6ae104379a716070a8e44
Rob Calcroft <rob@calcroft.com>
2015-10-13 23:46:22 +0100

    Added expiry support to tokens

--- lib/models/sql/AccessToken.sql
+++ lib/models/sql/AccessToken.sql
@@ -2,5 +2,6 @@ CREATE TABLE "AccessToken" (
     "accessToken" varchar(100) NOT NULL UNIQUE PRIMARY KEY,
     "clientId" varchar(100) REFERENCES "Client" ("clientId"),
     "userId" int REFERENCES "User" ("userId"),
+    "expiry" int NOT NULL,
     "dateCreated" timestamp DEFAULT CURRENT_TIMESTAMP
 );
