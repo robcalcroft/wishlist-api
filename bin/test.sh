d5d966a88b48485ca1b1e07127fb52bf72281277
Rob Calcroft <rob@calcroft.com>
2015-12-16 02:06:16 +0000

    Better test script

--- bin/test.sh
+++ bin/test.sh
@@ -2,8 +2,9 @@
 
 # Build test DB
 cd bin
-echo "> Building database"
+echo "> Building test database"
 ./db-build.sh wishlist-test > /dev/null
+./db-seed.sh wishlist-test > /dev/null
 cd ..
 
 # Start server and cache PID
@@ -24,9 +25,11 @@ unset WISHLIST_TEST
 unset NODE_PATH
 
 # Destroy test DB
+echo "> Removing test database"
 cd bin
 ./db-destroy.sh wishlist-test > /dev/null
 cd ..
 
 # Exit with the code of the Mocha script
+echo "> Exiting with code $EXIT_CODE"
 exit $EXIT_CODE
