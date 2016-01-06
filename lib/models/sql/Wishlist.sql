04acfc9b792dea242146efda5da75a887b5908ea
Rob Calcroft <rob@calcroft.com>
2016-01-06 23:36:31 +0000

    Added title column to wishlist table

--- lib/models/sql/Wishlist.sql
+++ lib/models/sql/Wishlist.sql
@@ -1,7 +1,8 @@
 CREATE TABLE "Wishlist" (
     "userId" int REFERENCES "User" ("userId"),
     "wishlistId" serial NOT NULL UNIQUE,
+    "title" varchar(100) NOT NULL UNIQUE,
     "dateCreated" timestamp DEFAULT CURRENT_TIMESTAMP,
     "isDefault" boolean NOT NULL,
-    "imageURI" varchar(255)
+    "imageURI" varchar(500)
 );
--- test/wishlist-data.sql
+++ test/wishlist-data.sql
@@ -1,10 +1,22 @@
 INSERT INTO "Wishlist" (
     "userId",
+    "title",
     "isDefault",
     "imageURI"
 )
 VALUES (
     '1',
+    'Main Wishlist',
     '1',
     'https://www.drupal.org/files/issues/header_1.png'
+), (
+    '1',
+    'Birthday',
+    '0',
+    'http://ui.stage.bidsystem.ak-networks.com/marketingSite/wp-content/themes/dt-nimble/images/backgrounds/header/art-header-main/full/r_atr-header-main.jpg'
+), (
+    '1',
+    'New House',
+    '0',
+    'http://www.freeliquidtemplates.com/free-headers-for-websites/header10.jpg'
 );
