e5bcb2f075c1c042c29ca9cfe3c290d9bb7cda55
Rob Calcroft <rob@calcroft.com>
2016-01-24 18:29:05 +0000

    Made more robust

--- lib/models/sql/Wishlist.sql
+++ lib/models/sql/Wishlist.sql
@@ -1,10 +1,10 @@
 CREATE TABLE "Wishlist" (
     "userId" int REFERENCES "User" ("userId"),
-    "wishlistId" serial NOT NULL UNIQUE,
+    "wishlistId" serial NOT NULL UNIQUE PRIMARY KEY,
     "title" varchar(100) NOT NULL UNIQUE,
     "dateCreated" timestamp DEFAULT CURRENT_TIMESTAMP,
     "isDefault" boolean NOT NULL,
     "imageURI" varchar(500),
-    "privacy" varchar(500) NOT NULL,
+    "privacy" varchar(100) NOT NULL,
     CONSTRAINT chk_privacy CHECK (privacy IN ('public', 'private'))
 );
