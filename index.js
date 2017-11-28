2d6ba85224574c54ec11b19c16429276702be776
Rob Calcroft <rob@calcroft.com>
2017-11-28 20:52:26 +0000

    Fixes

--- index.js
+++ index.js
@@ -5,19 +5,15 @@
  */
 
 // ES5 code here, subsequent requires are done using Babel
-var chalk = require('chalk');
+const chalk = require('chalk');
 
-console.log(
-    chalk.cyan('\n> Starting Up')
-);
+console.log(chalk.cyan('\n> Starting Up'));
 
 require('babel-core/register');
 
 // Load environment
 require('dotenv').load();
 
-console.log(
-    chalk.cyan('> Transpiling Code')
-);
+console.log(chalk.cyan('> Transpiling Code'));
 
 require('./lib/controllers');
